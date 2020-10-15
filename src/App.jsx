bfa6b8e03682334aca5df3b955c4c189ce48aeb3
Alvin Mantovani <alvin.ictn@gmail.com>
2020-10-15 21:08:13 +0700

    init files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  Button,
+  Image,
+  Badge,
+  
+} from 'react-bootstrap'
+
+import doctorImg from './assets/img/doctorProfile.png'
 
 function App() {
   return (
@@ -19,6 +32,22 @@ function App() {
           Learn React
         </a>
       </header>
+      <Container>
+        <Row>
+        <Col lg={4}>
+          <Card>
+            <Image style={{width:"50px",height:"50px"}}src={doctorImg} roundedCircle />
+            <h4>Alexandria Raihan</h4>
+            <Badge variant="warning">Doctor</Badge>
+          </Card>
+          <Card>te</Card>
+        </Col>
+        <Col lg={8}>
+          <Card>test</Card>
+        </Col>
+        </Row>
+        
+      </Container>
     </div>
   );
 }
