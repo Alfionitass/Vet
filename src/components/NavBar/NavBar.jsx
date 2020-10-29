574dc263ba511206e1f230824b2f242bd94c5b14
Alvin Mantovani <alvin.ictn@gmail.com>
2020-10-30 00:58:26 +0700

    update navmenu

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 // import { withRouter } from "react-router";
 import { Navbar, Nav, Image } from "react-bootstrap";
-import { Link, NavLink } from "react-router-dom";
-import logo from "../../assets/img/logo.png";
+import { Link } from "react-router-dom";
 import styles from "./NavBar.module.css";
 import { VetLogoSymbol } from "../../assets/icons";
+import NavMenu from './NavMenu'
 
 export default function NavBar() {
   const [state, setstate] = useState(window.location.pathname);
@@ -19,26 +19,7 @@ export default function NavBar() {
       <span className={styles.span}>VET</span>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <div className="ml-auto d-flex flex-row">
-          <NavLink to="/" className={styles.link}>
-            <span>Home</span>
-          </NavLink>
-          <NavLink to="/clinic" className={styles.link}>
-            <span>Clinic Chat</span>
-          </NavLink>
-          <NavLink to="/doctor" className={styles.link}>
-            <span>Doctor</span>
-          </NavLink>
-          <NavLink to="/ClinicChoose" className={styles.link}>
-            <span>Find a Clinic</span>
-          </NavLink>
-          <NavLink to="/auth/" className={styles.link}>
-            <span>Register</span>
-          </NavLink>
-          <NavLink to="/auth/login" className={styles.link}>
-            <span>Login</span>
-          </NavLink>
-        </div>
+        <NavMenu/>
       </Navbar.Collapse>
     </Navbar>
   );
---
+++ src/components/NavBar/NavMenu.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {  NavLink } from "react-router-dom";
+import styles from "./NavBar.module.css";
+
+export default function NavMenu() {
+  return (
+    <div className="ml-auto d-flex flex-row">
+      <NavLink to="/" className={styles.link}>
+        <span>Home</span>
+      </NavLink>
+      <NavLink to="/clinic" className={styles.link}>
+        <span>Clinic Chat</span>
+      </NavLink>
+      <NavLink to="/doctor" className={styles.link}>
+        <span>Doctor</span>
+      </NavLink>
+      <NavLink to="/ClinicChoose" className={styles.link}>
+        <span>Find a Clinic</span>
+      </NavLink>
+      <NavLink to="/auth/" className={styles.link}>
+        <span>Register</span>
+      </NavLink>
+      <NavLink to="/auth/login" className={styles.link}>
+        <span>Login</span>
+      </NavLink>
+    </div>
+  );
+}
