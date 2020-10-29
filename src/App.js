2675dadf27c178ebbcfe469447efff70d3735420
Qudsy <ucisolahuddin@gmail.com>
2020-10-29 10:39:28 +0700

    responsive

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
 import React from "react";
 import "./App.css";
 import Home from './components/Home/Home';
+//import Chat from './components/Chat/Chat';
+import {
+  BrowserRouter as Router,
+  Switch,  
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
   <div>
-  	Hello World
-    <Home/>
+     <Router>
+        <div>
+        <ul className="navpage">
+            <li>
+              <Link to="/Home">Home</Link>
+            </li>
+            <li>
+              <Link to="/Chat">Chat</Link>
+            </li>
+            
+          </ul>
+    
+
+        
+        <Switch>
+          {/* <Route path="/Chat">
+            <Chat />
+          </Route> */}
+          
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        </div>
+      </Router>
+  	
+    
   </div>
 
   );
