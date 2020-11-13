9677ce5fc581f0ad5c479828024ab9306cbf836d
andreyudi10 <andrianus.yudi@gmail.com>
2020-11-13 16:21:06 +0700

    benerin qudsy

--- src/components/Clinic/ClinicChoose/ClinicSearch.jsx
+++ src/components/Clinic/ClinicChoose/ClinicSearch.jsx
@@ -131,7 +131,7 @@ export default function ClinicSearch(){
           <Row >      
             {kartu}
           </Row>    
-          <Pagination />      
+          {/* <Pagination />       */}
         </div>    
       );
 }
